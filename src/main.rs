7eb3dbff8c458632afa11b96dec0810d1449ae46
Tom van Neerijnen <tom@tomvn.com>
2016-11-03 14:04:49 +0000

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,17 @@
+extern crate iron;
+
+use iron::prelude::*;
+use iron::status;
+use std::env;
+
+fn main() {
+    fn hello_world(_: &mut Request) -> IronResult<Response> {
+        Ok(Response::with((status::Ok, "Change me!")))
+    }
+
+    let url = format!("0.0.0.0:{}", env::var("PORT").unwrap());
+
+    println!("Binding on {:?}", url);
+    Iron::new(hello_world).http(&url[..]).unwrap();
+    println!("Bound on {:?}", url);
+}
